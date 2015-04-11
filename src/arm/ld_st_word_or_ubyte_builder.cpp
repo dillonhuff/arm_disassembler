0c51ce331d264b0dc11e5556d767e0baf340ad20
Dillon Huff <dillonhuff@gmail.com>
2015-04-11 07:18:07 +0000

    added arm obj and its dump to repo

--- src/arm/ld_st_word_or_ubyte_builder.cpp
+++ src/arm/ld_st_word_or_ubyte_builder.cpp
@@ -4,6 +4,7 @@ instruction ld_st_word_or_ubyte_builder::build_instruction(bit_field* instr_bits
   auto instr = instruction();
   auto bf = *instr_bits;
   instr.set_instr_class(LOAD_STORE_WD_OR_UB);
+  instr.set_width(4);
   instr.set_lds_b(bf[22]);
   instr.set_lds_l(bf[20]);
   return instr;
